5ce3958741539b1eaaad4caf7e783b4fc752133f
Casey Bui <buikhacnam11@gmail.com>
2023-04-25 06:59:57 +0700

    1st commit and unit test

---
+++ README.md
@@ -0,0 +1,31 @@
+### Get started
+
+```bash
+yarn dev
+```
+Username: `johndoe` | Password: `s3cret`
+
+
+### Why you should test your React app
+
+- You can make sure that your app works as expected
+- Safeguard against unwanted behavior when changes are made
+- Automated, and thus efficient on the long run
+
+### What to test
+1. High value features
+2. Edge cases in high value features
+3. Things that are easy to break
+4. Basic React component testing
+	- User interaction
+	- Conditional Rendering
+	- Utility functions / hooks
+
+
+
+### Unit Testing
+
+Please check the following files for unit testing examples:
+
+https://github.com/buikhacnam/react-testing/blob/main/src/components/TransactionCreateStepTwo.test.js
+
---
+++ src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,60 @@
+/* eslint-disable prettier/prettier */
+// reference: https://youtu.be/OVNjsIto9xM?t=686
+import { screen, render } from '@testing-library/react'
+import TransactionCreateStepTwo from './TransactionCreateStepTwo'
+import userEvent from '@testing-library/user-event'
+
+// Arrange - Act - Assert pattern
+
+// Arrange - setup the component (render it)
+// Act - interact with the component (click, type, etc)
+// Assert - check if the component is working as expected (check if the pay button is enabled)
+
+// check if the pay button is disabled in initial state
+test('check if the pay button is disabled in initial state', async () => {
+	// use async here because the button is first enabled and then disabled
+	render(
+		<TransactionCreateStepTwo
+			sender={{
+				id: '1',
+			}}
+			receiver={{
+				id: '2',
+			}}
+		/>
+	)
+
+	// sreen.debug() will print the html of the component
+	// check if the pay button is disabled in initial state
+	const payButton = await screen.findByRole('button', { name: /pay/i })
+	expect(payButton).toBeDisabled()
+})
+
+test('if the amount and note is entered, the pay button should be enabled', async () => {
+	render(
+		<TransactionCreateStepTwo
+			sender={{
+				id: '1',
+			}}
+			receiver={{
+				id: '2',
+			}}
+		/>
+	)
+
+	// screen.getByRole('') // print all the roles in the component
+
+	// get element by placeholder
+	const amountInput = screen.getByPlaceholderText(/amount/i)
+	const noteInput = screen.getByPlaceholderText(/add a note/i)
+
+	// enter amount and note
+	userEvent.type(amountInput, '100')
+	userEvent.type(noteInput, 'lunch')
+
+	// screen.getByRole('') // check if the amount and note is entered
+
+	// check if the pay button is enabled
+	const payButton = await screen.findByRole('button', { name: /pay/i })
+	expect(payButton).toBeEnabled()
+})
