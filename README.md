c0efae28841c03fbc8e247a2a0fbbd949e349ae9
Casey Bui <buikhacnam11@gmail.com>
2023-04-29 06:34:29 +0700

    e2e

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Get started
+## Get started
 
 ```bash
 yarn dev
@@ -6,13 +6,13 @@ yarn dev
 Username: `johndoe` | Password: `s3cret`
 
 
-### Why you should test your React app
+## Why you should test your React app
 
 - You can make sure that your app works as expected
 - Safeguard against unwanted behavior when changes are made
 - Automated, and thus efficient on the long run
 
-### What to test
+## What to test
 1. High value features
 2. Edge cases in high value features
 3. Things that are easy to break
@@ -23,14 +23,48 @@ Username: `johndoe` | Password: `s3cret`
 
 
 
-### Unit Testing
+## Unit Testing
 
 Please check the following component for unit testing examples:
 
 https://github.com/buikhacnam/react-testing/blob/main/src/components/TransactionCreateStepTwo.test.js
 
-### Integration Testing
+## Integration Testing
 
 Please check the following component for integration testing examples:
 
 https://github.com/buikhacnam/react-testing/blob/main/src/components/TransactionCreateStepTwo.test.js
+
+## End-to-end Testing
+
+Install `cypress`
+
+```bash
+yarn add -D cypress @testing-library/cypress
+```
+
+Open cypress
+
+```bash
+yarn cypress open
+```
+
+In the cyprss folder, go to `e2e` folder, create a new file `payment_spec.cy.js`
+
+Start writing the test
+
+```js
+describe('Login', () => {
+	it('should login an existing user', () => {
+		//  login
+		cy.visit('http://localhost:3000/signin')
+		cy.findByRole('textbox', { name: /username/i }).type('johndoe')
+		cy.findByLabelText(/password/i).type('s3cret')
+		cy.findByRole('checkbox', { name: /remember me/i }).check()
+		cy.findByRole('button', { name: /sign in/i }).click()
+	})
+})
+```
+And run the test on the cypress window
+
+* use `Testing Playground` Chrome extension can help to find the element
--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,8 +1,6 @@
-/* eslint-disable prettier/prettier */
-// reference: https://youtu.be/OVNjsIto9xM?t=686
-import { screen, render } from '@testing-library/react'
-import TransactionCreateStepTwo from './TransactionCreateStepTwo'
-import userEvent from '@testing-library/user-event'
+import { screen, render } from "@testing-library/react";
+import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
+import userEvent from "@testing-library/user-event";
 
 // Arrange - Act - Assert pattern
 
@@ -11,84 +9,83 @@ import userEvent from '@testing-library/user-event'
 // Assert - check if the component is working as expected (check if the pay button is enabled)
 
 // check if the pay button is disabled in initial state
-test('check if the pay button is disabled in initial state', async () => {
-	// use async here because the button is first enabled and then disabled
-	render(
-		<TransactionCreateStepTwo
-			sender={{
-				id: '1',
-			}}
-			receiver={{
-				id: '2',
-			}}
-		/>
-	)
+test("check if the pay button is disabled in initial state", async () => {
+  // use async here because the button is first enabled and then disabled
+  render(
+    <TransactionCreateStepTwo
+      sender={{
+        id: "1",
+      }}
+      receiver={{
+        id: "2",
+      }}
+    />
+  );
 
-	// sreen.debug() will print the html of the component
-	// check if the pay button is disabled in initial state
-	const payButton = await screen.findByRole('button', { name: /pay/i })
-	expect(payButton).toBeDisabled()
-})
+  // sreen.debug() will print the html of the component
+  // check if the pay button is disabled in initial state
+  const payButton = await screen.findByRole("button", { name: /pay/i });
+  expect(payButton).toBeDisabled();
+});
 
-test('if the amount and note is entered, the pay button should be enabled', async () => {
-	render(
-		<TransactionCreateStepTwo
-			sender={{
-				id: '1',
-			}}
-			receiver={{
-				id: '2',
-			}}
-		/>
-	)
+test("if the amount and note is entered, the pay button should be enabled", async () => {
+  render(
+    <TransactionCreateStepTwo
+      sender={{
+        id: "1",
+      }}
+      receiver={{
+        id: "2",
+      }}
+    />
+  );
 
-	// screen.getByRole('') // print all the roles in the component
+  // screen.getByRole('') // print all the roles in the component
 
-	// get element by placeholder
-	const amountInput = screen.getByPlaceholderText(/amount/i)
-	const noteInput = screen.getByPlaceholderText(/add a note/i)
+  // get element by placeholder
+  const amountInput = screen.getByPlaceholderText(/amount/i);
+  const noteInput = screen.getByPlaceholderText(/add a note/i);
 
-	// enter amount and note
-	userEvent.type(amountInput, '100')
-	userEvent.type(noteInput, 'lunch')
+  // enter amount and note
+  userEvent.type(amountInput, "100");
+  userEvent.type(noteInput, "lunch");
 
-	// screen.getByRole('') // check if the amount and note is entered
+  // screen.getByRole('') // check if the amount and note is entered
 
-	// check if the pay button is enabled
-	const payButton = await screen.findByRole('button', { name: /pay/i })
-	expect(payButton).toBeEnabled()
-})
-
+  // check if the pay button is enabled
+  const payButton = await screen.findByRole("button", { name: /pay/i });
+  expect(payButton).toBeEnabled();
+});
 
 //Integration Test
-// describe how a user whould use the app 
-test ('if the amount and note is entered, the pay button should be changed from disabled to enabled', async () => {
-	render(
-		<TransactionCreateStepTwo
-			sender={{
-				id: '1',
-			}}
-			receiver={{
-				id: '2',
-			}}
-		/>
-	)
+// describe how a user whould use the app
+test("if the amount and note is entered, the pay button should be changed from disabled to enabled", async () => {
+  render(
+    <TransactionCreateStepTwo
+      sender={{
+        id: "1",
+      }}
+      receiver={{
+        id: "2",
+      }}
+    />
+  );
 
-	let payButton = await screen.findByRole('button', { name: /pay/i })
+  let payButton = await screen.findByRole("button", { name: /pay/i });
 
-	// check if the pay button is disabled in initial state
-	expect(payButton).toBeDisabled()
+  // check if the pay button is disabled in initial state
+  expect(payButton).toBeDisabled();
 
-	// get element by placeholder
-	const amountInput = screen.getByPlaceholderText(/amount/i)
-	const noteInput = screen.getByPlaceholderText(/add a note/i)
+  // get element by placeholder
+  const amountInput = screen.getByPlaceholderText(/amount/i);
+  const noteInput = screen.getByPlaceholderText(/add a note/i);
 
-	// enter amount and note
-	userEvent.type(amountInput, '100')
-	userEvent.type(noteInput, 'lunch')
+  // enter amount and note
+  userEvent.type(amountInput, "100");
+  userEvent.type(noteInput, "lunch");
 
-	payButton = await screen.findByRole('button', { name: /pay/i })
+  payButton = await screen.findByRole("button", { name: /pay/i });
 
-	// check if the pay button is enabled
-	expect(payButton).toBeEnabled()
-})
+  // check if the pay button is enabled
+  expect(payButton).toBeEnabled();
+});
